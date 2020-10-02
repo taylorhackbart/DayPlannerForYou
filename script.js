ad76c79e3561c8526d82837192efff701bb92cd0
Taylor <taylor.hackbart@du.edu>
2020-10-02 14:34:32 -0600

    added more comments

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+//variables 
 var hour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var displayHour = ["9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
 
+//current day from moment, with specific formatting
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
 
+//creating a loop to loop through the hours of the day, and the current time
 for (i=0; i<hour.length; i++){
     var createDiv = $( ` <div id = "hour-${hour[i]}" class = "row time-block" value = ${hour[i]} > ` );
     var createTextArea = $("<textarea>");
     $(".container").append(createDiv);
     var currentTime = displayHour[i];
+    
+    //going into the new Div row and creating columns for each attribute
     createDiv.append($("<div class = 'col-md-1 hour' >").text(currentTime));
     createDiv.append(createTextArea.attr("class", "col-md-10 description").attr("id", "memo-area"));
     createDiv.append($("<button class = 'col-md-1 saveBtn button' >").text("save"));
     
-    console.log(hour[i]);
-
+    // displaying if time blocks are in the past, present, or future
     if(moment().format("H") == hour[i]){
         $("#hour-"+ hour[i]).addClass("present")
         } else if(moment().format("H")> hour[i]){
@@ -22,11 +26,13 @@ for (i=0; i<hour.length; i++){
         $("#hour-"+ hour[i]).addClass("future")
     }    
 }
+//creating buttons for the save column
 $(".saveBtn").on("click", function(){
     var value = $(this).siblings(".description").val();
     var key = $(this).parent().attr("id");
     localStorage.setItem(key, value);
 })
+//adding into local storage
 $("#hour-9 .description").val(localStorage.getItem("hour-9"))
 $("#hour-10 .description").val(localStorage.getItem("hour-10"))
 $("#hour-11 .description").val(localStorage.getItem("hour-11"))
